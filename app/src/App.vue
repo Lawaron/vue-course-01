94ccda563d1b657898a1e47cc1949c5ecd262848
Somogyi Ervin <ervin.somogyi@gmail.com>
2025-08-29 16:22:37 +0200

    practice

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,15 +1,8 @@
 <script setup>
-  import getBooks from '@/composables/getBooks'
-
-  const { load } = getBooks()
-
-  load()
+  import BooksList from '@/components/BooksList.vue'
 </script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>Books</h1>
+  <BooksList />
 </template>
---
+++ app/src/components/BooksList.vue
@@ -0,0 +1,17 @@
+<script setup>
+  import getBooks from '@/composables/getBooks'
+
+  const { books, load } = getBooks()
+
+  load()
+</script>
+<template>
+  <ol>
+    <li v-for="{ id, author, title, tags } in books" :key="id">
+      <h2>{{ title }}</h2>
+      <h3>{{ author }}</h3>
+      <h4>{{ tags }}</h4>
+      <h5>{{ id }}</h5>
+    </li>
+  </ol>
+</template>
